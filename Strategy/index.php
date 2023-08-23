c929956985b189a5e1e67c9538f9bbb6437006df
0xfee1dead <wk.pavel@mail.ru>
2023-08-23 20:53:54 +0800

    add strategy design pattern

---
+++ Strategy/index.php
@@ -0,0 +1,54 @@
+<?php
+
+interface LogInterface
+{
+    public function log(string $message) : void;
+}
+
+class FileLog implements LogInterface
+{
+    public function log(string $message) : void
+    {
+        echo "log \"$message\" to file" . PHP_EOL;
+    }
+}
+
+class DatabaseLog implements LogInterface
+{
+    public function log(string $message) : void
+    {
+        echo "log \"$message\" to database" . PHP_EOL;
+    }
+}
+
+class App
+{
+    protected ?LogInterface $logger = null;
+
+    public function setLogger(?LogInterface $logger=null) : self
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+
+    public function log(string $message) : void
+    {
+        if(!is_null($this->logger))
+        {
+            $this->logger->log($message);
+        }
+    }
+}
+
+$app = new App;
+
+$app->log('Log without logger');
+
+$app->setLogger(new FileLog);
+
+$app->log('log with file log');
+
+$app->setLogger(new DatabaseLog);
+
+$app->log('log to database log');
